f0cab5d50bb8d2dbd5ea4b5b1dc01f2925dc18da
cynthiaangi <cynthia.angi@sptech.school>
2024-11-11 14:13:26 -0300

    adicionando validações

---
+++ tabelas/script.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE individual;
+
+USE individual;
+
+CREATE TABLE cadastro(
+idCadastro INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(45),
+dtNasc DATE,
+email VARCHAR(45),
+telefone CHAR(14),
+genero CHAR(8)
+constraint chkGen CHECK(genero IN('Feminino'))
+);
+
+DESC cadastro;
+
+CREATE TABLE historia(
+idHistoria INT,
+fkUser INT,
+constraint pkComposta PRIMARY KEY(idHistoria, fkUser),
+dtAborto DATE,
+tempo INT,
+motivo VARCHAR(45),
+descricao VARCHAR(200),
+autorizacao CHAR(3)
+constraint chkAut CHECK(autorizacao IN('Sim', 'Não')),
+constraint fkUserHist
+FOREIGN KEY (fkUser) references cadastro(idCadastro)
+);
+
+DESC historia;
