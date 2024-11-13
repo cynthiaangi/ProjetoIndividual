168875e9fb96e219ab5c5ee4aaf70e07d28ab732
cynthiaangi <cynthia.angi@sptech.school>
2024-11-12 23:29:45 -0300

    criação do conte sua historia

---
+++ src/database/script-tabelas.sql
@@ -0,0 +1,77 @@
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+
+/*
+comandos para mysql server
+*/
+
+CREATE DATABASE aquatech;
+
+USE aquatech;
+
+CREATE TABLE empresa (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	razao_social VARCHAR(50),
+	cnpj CHAR(14),
+	codigo_ativacao VARCHAR(50)
+);
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50),
+	fk_empresa INT,
+	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+);
+
+CREATE TABLE aviso (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(100),
+	descricao VARCHAR(150),
+	fk_usuario INT,
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+);
+
+create table aquario (
+/* em nossa regra de negócio, um aquario tem apenas um sensor */
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	descricao VARCHAR(300),
+	fk_empresa INT,
+	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+);
+
+/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+
+create table medida (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	dht11_umidade DECIMAL,
+	dht11_temperatura DECIMAL,
+	luminosidade DECIMAL,
+	lm35_temperatura DECIMAL,
+	chave TINYINT,
+	momento DATETIME,
+	fk_aquario INT,
+	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+);
+
+insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
+insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
+insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
+insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
+
+select * from usuario;
+
+alter table usuario add column cpf char(14);
+
+insert into medida (dht11_umidade, dht11_temperatura, momento, fk_aquario) VALUES
+	(85, 20, '2024-11-07 23:21:00', 1),
+    (80, 26, '2024-11-07 23:23:00', 1),
+    (87, 16, '2024-11-07 23:25:00', 1),
+    (82, 23, '2024-11-07 23:27:00', 1),
+    (89, 20, '2024-11-07 23:29:00', 1),
+    (81, 18, '2024-11-07 23:31:00', 1);
+    
+insert into medida (dht11_umidade, dht11_temperatura, momento, fk_aquario) VALUES
+	(85, 28, '2024-11-07 23:33:00', 1);
